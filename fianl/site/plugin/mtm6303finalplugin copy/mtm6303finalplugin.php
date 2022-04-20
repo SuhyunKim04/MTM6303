ced7c6e879fae2b183fa7208a442f422d77eb723
SuhyunKim04 <77449435+SuhyunKim04@users.noreply.github.com>
2022-04-20 19:29:41 -0400

    Add files via upload

---
+++ fianl/site/plugin/mtm6303finalplugin copy/mtm6303finalplugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package MTM6303Plugin
+ */
+/*
+Plugin Name: MTM6303 Plugin
+Plugin URI: http://algonquincollege.com/
+Description:This is a Plugin created for MTM6303 class
+Version: 1.0.0
+Author: Suhyun Kim
+Author URI: http://algonquincollege.com/
+License: GPLv2 or later
+Text Domain: mtm6303Plugin
+*/
+
